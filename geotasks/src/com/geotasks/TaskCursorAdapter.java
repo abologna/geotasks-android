2c9386d6b39ee6bd3e19bee360c4c92087d8f421
abologna <bologna.alejandro@gmail.com>
2011-05-14 15:33:15 -0300

    apply strike-through style on completed tasks

--- geotasks/src/com/geotasks/TaskCursorAdapter.java
+++ geotasks/src/com/geotasks/TaskCursorAdapter.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.text.SpannableString;
+import android.text.style.StrikethroughSpan;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
@@ -30,14 +32,23 @@ public class TaskCursorAdapter extends SimpleCursorAdapter {
   }
   
   @Override
-  public void bindView(View view, final Context context, Cursor cursor) {
-    TextView name = (TextView) view.findViewById(R.id.taskName);
-    CheckBox complete = (CheckBox) view.findViewById(R.id.taskComplete);
-
-    String nameText = cursor.getString(cursor.getColumnIndex(Tasks.NAME));
-    name.setText(nameText);
+  public void bindView(final View view, final Context context, Cursor cursor) {
+    final TextView name = (TextView) view.findViewById(R.id.taskName);
+    CheckBox complete = (CheckBox) view.findViewById(R.id.taskComplete);   
+    
     int completed = cursor.getInt(cursor.getColumnIndex(Tasks.COMPLETED));
     complete.setChecked(completed == 0 ? false : true);
+    
+    // If task completed apply strikethrough style to the text.
+    final String nameText = cursor.getString(cursor.getColumnIndex(Tasks.NAME));
+    if (completed != 0) {
+      SpannableString content = new SpannableString(nameText);
+      content.setSpan(new StrikethroughSpan(), 0, content.length(), 0);
+      name.setText(content);
+    } else {
+      name.setText(nameText);
+    }
+    
     final long taskId = cursor.getLong(cursor.getColumnIndex(Tasks._ID));
 
     view.setOnClickListener(new OnClickListener() {
@@ -53,6 +64,16 @@ public class TaskCursorAdapter extends SimpleCursorAdapter {
         values.put(Tasks.COMPLETED, isChecked);
         Uri uri = ContentUris.withAppendedId(Tasks.CONTENT_URI, taskId);
         context.getContentResolver().update(uri, values, null, null);
+        
+        // Apply or remove strikethrough style depending on isChecked value.
+        SpannableString content = new SpannableString(nameText);
+        if (isChecked) {
+          content.setSpan(new StrikethroughSpan(), 0, content.length(), 0);
+        } else {
+          content.removeSpan(new StrikethroughSpan());
+        }
+        name.setText(content);
+        
       }
     });
   }
